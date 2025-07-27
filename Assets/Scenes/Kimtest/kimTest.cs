b7d36ea988834084ce7f896ce8e6c80dbffbb595
Howdiy <kimmik72915@gmail.com>
2025-07-27 20:53:30 +0900

    브랜치 테스트

--- Assets/Scenes/Kimtest/kimTest.cs
+++ Assets/Scenes/Kimtest/kimTest.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,7 @@ public class kimTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string test;
     }
 
     // Update is called once per frame
